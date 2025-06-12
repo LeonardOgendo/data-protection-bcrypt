0b15ee324327946bdea0fd47e7135c0800253230
Leonard Ogendo <ogendoleonard5@gmail.com>
2025-06-12 14:22:50 +0300

    Password hashing and comparison done

--- server.js
+++ server.js
@@ -3,23 +3,41 @@ const express     = require('express');
 const bodyParser  = require('body-parser');
 const fccTesting  = require('./freeCodeCamp/fcctesting.js');
 const app         = express();
+const bcrypt      = require('bcrypt');
 fccTesting(app);
+
 const saltRounds = 12;
 const myPlaintextPassword = 'sUperpassw0rd!';
 const someOtherPlaintextPassword = 'pass123';
 
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
-
-
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+    if (err) {
+        console.error('Error hashing password:', err);
+    } else {
+        console.log('Hashed password:', hash);
+        
+        bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+            if (err) {
+            console.error('Error comparing password:', err);
+            } else {
+            console.log('Password match:', res);
+            }
+        });
+    }
+});
 
 //END_ASYNC
 
 //START_SYNC
+var hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
+console.log('Hashed password:', hash);
 
-
-
+var result = bcrypt.compareSync(myPlaintextPassword, hash);
+console.log(result);
 //END_SYNC
+
 
 
 
